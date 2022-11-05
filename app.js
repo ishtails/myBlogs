51d7de3f3560fe7748eaad3fa949a3545af6f208
Styles <kartik100100@gmail.com>
2022-11-05 14:26:07 +0530

    meh

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect(dbURI)
 //Express
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
-app.use(express.urlencoded({extended: true}));
+app.use(urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
